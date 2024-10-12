745830e1d5cb47cda00b5d489ce1663f82c761ad
JonasMoser2006 <j.moser1@students.htl-leonding.ac.at>
2024-10-12 10:18:53 +0200

    added binary search method

--- CCC_Temp_Project/CCC_Temp_Project/Program.cs
+++ CCC_Temp_Project/CCC_Temp_Project/Program.cs
@@ -5,8 +5,62 @@ namespace CccTempProject
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Temp Project");
         }
+
+        public static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+
+                if (array[middle] == target)
+                {
+                    return middle;
+                }
+                else if (array[middle] < target)
+                { 
+                    left = middle + 1;
+                }
+                else
+                { 
+                    right = middle - 1;    
+                }
+            }
+
+            return -1; // if id isnt found
+        }
+
+        public static int BinarySearchFromObject(TempClass[] obj, int id)
+        {
+            int left = 0;
+            int right = obj.Length - 1;
+            
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+
+                if (obj[middle].getId() == id)
+                {
+                    return middle;
+                }
+                else if (obj[middle].getId() < id)
+                { 
+                    left = middle + 1;
+                }
+                else
+                { 
+                    right = middle - 1;    
+                }
+            }
+
+            return -1; // if id isnt found
+        }
+        
+        
     }
     
 }
---
+++ CCC_Temp_Project/CCC_Temp_Project/TempClass.cs
@@ -0,0 +1,17 @@
+namespace CccTempProject;
+
+public class TempClass
+{
+    private int ID;
+
+    public TempClass(int ID)
+    {
+        this.ID = ID;
+    }
+
+    public int getId()
+    {
+        return this.ID;
+    }
+    
+}
