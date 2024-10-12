5077c9c457cbb81855260d0f7bb3bde253557f24
JonasMoser2006 <j.moser1@students.htl-leonding.ac.at>
2024-10-12 10:39:41 +0200

    added more sample code

--- CCC_Temp_Project/CCC_Temp_Project/Program.cs
+++ CCC_Temp_Project/CCC_Temp_Project/Program.cs
@@ -59,6 +59,52 @@ namespace CccTempProject
 
             return -1; // if id isnt found
         }
+
+        static string AsciiToString(string asciiString)
+        {
+            string[] asciiValues = asciiString.Split(' ');
+            char[] characters = new char[asciiValues.Length];
+
+            for (int i = 0; i < asciiValues.Length; i++)
+            {
+                int asciiCode = int.Parse(asciiValues[i]);
+                characters[i] = (char)asciiCode; 
+            }
+
+            return new string(characters); 
+        }
+        
+        static string StringToAscii(string input)
+        {
+            int[] asciiValues = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                asciiValues[i] = (int)input[i]; 
+            }
+
+            return string.Join(" ", asciiValues);
+        }
+        
+        static string[] ReadFileAsLines(string filePath)
+        {
+            try
+            {
+                return File.ReadAllLines(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler beim Lesen der Datei: {ex.Message}");
+                return new string[0]; 
+            }
+        }
+        
+        static string[] SplitStringBySpaces(string input)
+        {
+            return input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+        
+        
         
         
     }
--- README.md
+++ README.md
@@ -3,9 +3,15 @@
 ## C# - Rider .net8.0 SDK 8.0
 
 ## Methoden:
-* Sortierung
+* Sortierung von ints
+* Sortierung von objekten (ohne compare) per id
 * **Einlesen von Files**
-* 1 x Temp Klasse
+* 1 x Temp Klasse (id als Parameter)
 * Verwendung von linked lists
 * convert Text to Ascii
-  
+* convert Ascii to Text
+* Binary Search
+
+## Temp Projekt:
+
+
