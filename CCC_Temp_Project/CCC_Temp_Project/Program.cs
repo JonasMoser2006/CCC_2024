2bcb31690d3d8d10f8e0c43f57e498a66be72c4c
JonasMoser2006 <j.moser1@students.htl-leonding.ac.at>
2024-10-12 11:49:56 +0200

    changed temp project

--- CCC_Temp_Project/CCC_Temp_Project/Program.cs
+++ CCC_Temp_Project/CCC_Temp_Project/Program.cs
@@ -9,32 +9,7 @@ namespace CccTempProject
         public static void Main(string[] args)
         {
             Console.WriteLine("Temp Project");
-
-            string path = "output/output.out";
-
-            string[] test =
-            {
-                "aaaa",
-                "BBBBB",
-                "CCCCC",
-                "DDDDD"
-            };
             
-            WriteToFile(path, test);
-        }
-        
-        
-        static void WriteToFile(string filePath, string[] lines)
-        {
-            try
-            {
-                //OUTPUT FILE MUSS .OUT ENDEN
-                File.WriteAllLines(filePath, lines);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Fehler beim Schreiben in die Datei: {ex.Message}");
-            }
         }
     }
 }
