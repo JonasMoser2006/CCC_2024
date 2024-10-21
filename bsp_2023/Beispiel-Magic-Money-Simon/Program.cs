c68a3e0f2ea25224993d09f8a48c8d9b68a12064
Simon Matulin <s.matulin@students.htl-leonding.ac.at>
2024-10-21 12:26:30 +0200

    added my solution for magic money

---
+++ bsp_2023/Beispiel-Magic-Money-Simon/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] data = File.ReadAllLines("../../../data/level1_1.in");
+
+        int n = Convert.ToInt32(data[0]);
+        int c = Convert.ToInt32(data[1]);
+
+        int[][] currencies = new int[n][];
+        
+        for (int i = 0; i < n; i++)
+        {
+            currencies[i] = data[i + 2].Split(' ').Select(int.Parse).ToArray();
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            int result = FindSmallestPossibleValue(currencies[i]);
+            Console.WriteLine(result);
+        }
+    }
+
+    public static int FindSmallestPossibleValue(int[] values)
+    {
+        int result = values[values.Length - 1] + 1;
+        bool hasSmallest = false;
+
+        for (int i = 0; i < values.Length && !hasSmallest; i++)
+        {
+            if (i + 1 < values.Length && values[i] != values[i + 1] - 1)
+            {
+                hasSmallest = true;
+                result = values[i] + 1;
+            }
+        }
+        
+        return result;
+    }
+}
