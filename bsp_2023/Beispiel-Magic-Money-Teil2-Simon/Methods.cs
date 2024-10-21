ae6f00490ec6370220f2a73f1d962654ff3d8800
Simon Matulin <s.matulin@students.htl-leonding.ac.at>
2024-10-21 19:02:08 +0200

    added solution for level 2

---
+++ bsp_2023/Beispiel-Magic-Money-Teil2-Simon/Methods.cs
@@ -0,0 +1,147 @@
+﻿namespace CCC_Test;
+
+public class Methods
+{
+    public static int BinarySearch(int[] array, int target)
+{
+    int left = 0;
+    int right = array.Length - 1;
+
+    while (left <= right)
+    {
+        int middle = left + (right - left) / 2;
+
+        if (array[middle] == target)
+        {
+            return middle;
+        }
+        else if (array[middle] < target)
+        { 
+            left = middle + 1;
+        }
+        else
+        { 
+            right = middle - 1;    
+        }
+    }
+
+    return -1; // if id isnt found
+}
+
+/*public static int BinarySearchFromObject(TempClass[] obj, int id)
+{
+    int left = 0;
+    int right = obj.Length - 1;
+    
+    while (left <= right)
+    {
+        int middle = left + (right - left) / 2;
+
+        if (obj[middle].getId() == id)
+        {
+            return middle;
+        }
+        else if (obj[middle].getId() < id)
+        { 
+            left = middle + 1;
+        }
+        else
+        { 
+            right = middle - 1;    
+        }
+    }
+
+    return -1; // if id isnt found
+}*/
+
+static string AsciiToString(string asciiString)
+{
+    string[] asciiValues = asciiString.Split(' ');
+    char[] characters = new char[asciiValues.Length];
+
+    for (int i = 0; i < asciiValues.Length; i++)
+    {
+        int asciiCode = int.Parse(asciiValues[i]);
+        characters[i] = (char)asciiCode; 
+    }
+
+    return new string(characters); 
+}
+
+static string StringToAscii(string input)
+{
+    int[] asciiValues = new int[input.Length];
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        asciiValues[i] = (int)input[i]; 
+    }
+
+    return string.Join(" ", asciiValues);
+}
+
+static string[] ReadFileAsLines(string filePath)
+{
+    try
+    {
+        return File.ReadAllLines(filePath);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Fehler beim Lesen der Datei: {ex.Message}");
+        return new string[0]; 
+    }
+}
+
+static string[] SplitStringBySpaces(string input)
+{
+    return input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+}
+
+static void InsertionSort(int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = key;
+    }
+}
+
+    /*static void InsertionSortById(TempClass[] temps)
+    {
+        for (int i = 1; i < temps.Length; i++)
+        {
+            TempClass key = temps[i];
+            int j = i - 1;
+
+            while (j >= 0 && temps[j].getId() > key.getId())
+            {
+                temps[j + 1] = temps[j];
+                j--;
+            }
+
+            temps[j + 1] = key; 
+        }
+    }*/
+
+    static void WriteToFile(string filePath, string[] lines)
+    {
+        try
+        {
+            //OUTPUT FILE MUSS .OUT ENDEN
+            File.WriteAllLines(filePath, lines);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler beim Schreiben in die Datei: {ex.Message}");
+        }
+    }
+}
---
+++ bsp_2023/Beispiel-Magic-Money-Teil2-Simon/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] data = File.ReadAllLines("../../../data/level2_3.in");
+
+        int n = Convert.ToInt32(data[0]);
+        int c = Convert.ToInt32(data[1]);
+        int a = Convert.ToInt32(data[2]);
+        
+        int[] values = new int[5];
+
+        int[] targets = new int[3];
+
+        for (int i = 3; i < data.Length; i+=2)
+        {
+            // Komplettes String array in Int array konvertieren
+            values = Array.ConvertAll(data[i].Split(" "), int.Parse);
+            targets = Array.ConvertAll(data[i + 1].Split(" "), int.Parse);
+            
+            CallFindTwoSum(values, targets);
+        }
+    }
+
+    public static void CallFindTwoSum(int[] values, int[] targets)
+    {
+        foreach (int target in targets)
+        {
+            int[] result = FindTwoSum(values, target);
+            Console.WriteLine($"{result[0]} + {result[1]} -> {target}");
+        }
+    }
+
+    public static int[] FindTwoSum(int[] values, int target)
+    {
+        int[] result = new int[2];
+        bool hasValues = false;
+
+        for (int i = 0; i < values.Length && !hasValues; i++)
+        {
+            // target = values[i] + x -> target - values[i] = x
+            if (values.Contains(target - values[i]))
+            {
+                result[0] = values[i];
+                result[1] = target - values[i];
+                hasValues = true;
+            }
+        }
+        
+        return result;
+    }
+}
